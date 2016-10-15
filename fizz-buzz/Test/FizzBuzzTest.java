92406b4a2b8f2a672ffb82734cbc3d89f30a14f8
jonayGodoy <jonaydam2@gmail.com>
2016-10-15 17:36:37 +0100

    FizzBuzzTest createArrayHundredNumbers not compile

---
+++ fizz-buzz/Test/FizzBuzzTest.java
@@ -0,0 +1,21 @@
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class FizzBuzzTest {
+
+
+    @Test
+    public void createArrayHundredNumbers(){
+        FizzBuzz fb = new FizzBuzz();
+
+        int [] listNumbers = new int[99];
+
+        assertEquals(100,listNumbers.length);
+    }
+
+
+
+
+
+}
